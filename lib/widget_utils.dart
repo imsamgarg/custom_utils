aed6dfcef7beb7f8772f1ff306c28c94beaf5bff
Sam Garg <smlgarg123@gmail.com>
2022-02-22 14:39:48 +0530

    widgets added

--- lib/widget_utils.dart
+++ lib/widget_utils.dart
@@ -2,3 +2,33 @@ import 'package:flutter/material.dart';
 
 const Widget kEmptyWidget = SizedBox.shrink();
 const kCircleBorder = CircleBorder();
+
+class FullWidthBox extends StatelessWidget {
+  const FullWidthBox({Key? key, this.child}) : super(key: key);
+
+  final Widget? child;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: child,
+    );
+  }
+}
+
+class WhiteContainer extends StatelessWidget {
+  const WhiteContainer({
+    Key? key,
+    this.child,
+  }) : super(key: key);
+
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    return ColoredBox(
+      color: Colors.white,
+      child: child,
+    );
+  }
+}
